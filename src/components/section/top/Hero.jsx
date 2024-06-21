e6f36a79222ac952c77a6982f77e873ec6274290
TakashiSuzukiDiverta <tsuzuki@diverta.co.jp>
2024-06-21 14:14:57 +0900

    Update Hero.jsx

--- src/components/section/top/Hero.jsx
+++ src/components/section/top/Hero.jsx
@@ -2,9 +2,9 @@ const Hero = () => {
   return (
     <section className='c-hero'>
       <div className='c-hero__body'>
-        <p className='c-hero__body__lead'>Let&apos;s Travel and Enjoy!</p>
+        <p className='c-hero__body__lead'>KL 良い店 飲める店</p>
         <p className='c-hero__body__text'>
-          旅に出よう！ホテル・グルメ・観光スポット お役立ち情報が満載！
+          マレーシア・クアラルンプール近郊の気になるあのお店。飲めるか飲めないか現地調査レポートします。
         </p>
       </div>
     </section>
