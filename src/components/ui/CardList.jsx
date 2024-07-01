a2d5a6ebb054458c49c9ac2d879cec0bffdf650a
TakashiSuzukiDiverta <tsuzuki@diverta.co.jp>
2024-07-02 08:18:48 +0900

    Update CardList.jsx

--- src/components/ui/CardList.jsx
+++ src/components/ui/CardList.jsx
@@ -15,10 +15,7 @@ export default function CardList({ data }) {
               <span className='c-card__image__badge'>
                 {card.contents_type_nm}
               </span>
-      const sqr = {
-        "padding-bottom" : '100%'
-              }
-              <div className='c-card__image' style={sqr}>
+              <div className='c-card__image' style={{padding-bottom: 100%}}>
                 <Image
                   alt={card.image.desc || 'dummy'}
                   src={card.image.url}
