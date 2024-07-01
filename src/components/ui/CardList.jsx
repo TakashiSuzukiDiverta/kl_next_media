aebe8a256dc5dbc51dc0dabca8d73dcb4ff37a76
TakashiSuzukiDiverta <tsuzuki@diverta.co.jp>
2024-07-02 08:39:29 +0900

    Update CardList.jsx

--- src/components/ui/CardList.jsx
+++ src/components/ui/CardList.jsx
@@ -5,6 +5,9 @@ export default function CardList({ data }) {
   if (!data || !Array.isArray(data)) {
     return null;
   }
+  const sqr = {
+    padding-bottom: "100%"
+  };
 
   return (
     <div>
@@ -15,7 +18,7 @@ export default function CardList({ data }) {
               <span className='c-card__image__badge'>
                 {card.contents_type_nm}
               </span>
-              <div className='c-card__image' style={{ padding-bottom: 100 + '%' }}>
+              <div className='c-card__image' style={sqr}>
                 <Image
                   alt={card.image.desc || 'dummy'}
                   src={card.image.url}
