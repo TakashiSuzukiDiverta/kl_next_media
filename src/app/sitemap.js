47a3fb6f11ac8a0151444c73858e9934cb2b1b3e
TakashiSuzukiDiverta <tsuzuki@diverta.co.jp>
2024-07-29 14:55:02 +0900

    Update sitemap.js

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -3,25 +3,25 @@ import getAllContentList from '@/fetch/getAllContentList';
 export default async function sitemap() {
   const items = await getAllContentList();
   const articleDetails = items.map((item) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/article/${item.topics_id}/`,
+    url: `https://www.kl-good.net/article/${item.topics_id}/`,
     lastModified: new Date(item.update_ymdhi),
   }));
 
   const features = await getAllContentList();
   const featureDetails = features.map((item) => ({
-    url: `${process.env.NEXT_PUBLIC_BASE_URL}/feature/${item.topics_id}/`,
+    url: `https://www.kl-good.net/feature/${item.topics_id}/`,
     lastModified: new Date(item.update_ymdhi),
   }));
 
   return [
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/privacy/`,
+      url: `https://www.kl-good.net/privacy/`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact/`,
+      url: `https://www.kl-good.net/contact/`,
     },
     {
-      url: `${process.env.NEXT_PUBLIC_BASE_URL}/article/`,
+      url: `https://www.kl-good.net/article/`,
     },
     ...articleDetails,
     ...featureDetails,
